692a5ea571469376e287ddb3342d1045e1574f35
Victor Gabriel <victorgabrielcc22@gmail.com>
2026-05-06 20:27:47 -0300

    fix: enhance create_driver function with additional preferences and disable features for improved stability

--- web/utils/driver_factory.py
+++ web/utils/driver_factory.py
@@ -13,16 +13,27 @@ def create_driver() -> "webdriver.Chrome":
     options.add_experimental_option(
         "prefs",
         {
+            "autofill.credit_card_enabled": False,
+            "autofill.profile_enabled": False,
             "credentials_enable_service": False,
             "profile.default_content_setting_values.geolocation": 2,
             "profile.default_content_setting_values.media_stream_camera": 2,
             "profile.default_content_setting_values.media_stream_mic": 2,
             "profile.default_content_setting_values.notifications": 2,
             "profile.default_content_settings.popups": 0,
+            "profile.password_manager_leak_detection": False,
             "profile.password_manager_enabled": False,
         },
     )
     options.set_capability("unhandledPromptBehavior", "dismiss")
+    options.add_argument(
+        "--disable-features="
+        "AutofillServerCommunication,"
+        "PasswordGeneration,"
+        "PasswordLeakDetection,"
+        "PasswordManagerEnabled,"
+        "PasswordManagerOnboarding"
+    )
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
